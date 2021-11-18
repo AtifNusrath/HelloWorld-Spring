c57c669b51c123a0be176412cad45b411edbaf82
atif00 <mohammedatif90@gmail.com>
2021-11-18 16:28:17 +0530

    Spring boot concept

--- src/main/java/com/example/demo/NewHelloApplication.java
+++ src/main/java/com/example/demo/NewHelloApplication.java
@@ -2,13 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.example.demo.component.DemoBean;
+import com.example.demo.controller.HelloRestController;
+
 
 @SpringBootApplication
 public class NewHelloApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Bridgelabz ");
-		SpringApplication.run(NewHelloApplication.class, args);
+		ApplicationContext context = SpringApplication.run(NewHelloApplication.class, args);
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        System.out.println("Demo Bean = " + demoBean.toString());
+        System.out.println(context.getBean(HelloRestController.class));
 	}
 
 }
