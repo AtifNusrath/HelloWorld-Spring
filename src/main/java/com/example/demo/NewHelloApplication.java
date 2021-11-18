c7e46088f19e9998138152cb4b075051fdd8ccb6
atif00 <mohammedatif90@gmail.com>
2021-11-18 17:15:12 +0530

    Added: Using EmployeeBean and Department Bean Checking Dependency Injection

--- src/main/java/com/example/demo/NewHelloApplication.java
+++ src/main/java/com/example/demo/NewHelloApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import com.example.demo.component.DemoBean;
+import com.example.demo.component.EmployeeBean;
 import com.example.demo.controller.HelloRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,12 @@ public class NewHelloApplication {
         DemoBean demoBean = context.getBean(DemoBean.class);
         logger.info("Demo Bean = " + demoBean.toString());
         System.out.println(context.getBean(HelloRestController.class));
+        logger.debug("Checking Context:{}", context.getBean(DemoBean.class));
+        logger.debug("\n*** Example using @Autowire annotation on property***");
+        EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+        employeeBean.seteId(101);
+        employeeBean.seteName("Mohammed");
+        employeeBean.showEmployeeDetails();
 	}
 
 }
